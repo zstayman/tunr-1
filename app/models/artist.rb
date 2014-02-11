163113eb68ff18d7010e83c406b5c639daf55bb6
h4w5 <philip@h4w5.com>
2014-02-11 09:18:41 -0500

    clean up but not change models

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,5 +1,7 @@
 class Artist < ActiveRecord::Base
   has_many :songs, dependent: :destroy
+  
   validates :name, presence: true
   validates :photo_url, presence: true
+
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,7 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+
   validates :title, presence: true
-  validates :artist_id, presence: true
+  validates :artist, presence: true
+
 end
