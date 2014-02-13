b41398a8243a429da42cdbeeaa4535846fc6223f
Jeffrey Konowitch <jeff.konowitch@generalassemb.ly>
2014-02-13 17:01:54 -0500

    purchasing domain logic

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140213213358) do
+ActiveRecord::Schema.define(version: 20140213214551) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,13 +36,14 @@ ActiveRecord::Schema.define(version: 20140213213358) do
   end
 
   create_table "users", force: true do |t|
-    t.string "email"
-    t.string "first_name"
-    t.string "last_name"
-    t.date   "dob"
-    t.string "gender"
-    t.string "facebook_link"
-    t.string "password_digest"
+    t.string  "email"
+    t.string  "first_name"
+    t.string  "last_name"
+    t.date    "dob"
+    t.string  "gender"
+    t.string  "facebook_link"
+    t.string  "password_digest"
+    t.decimal "balance"
   end
 
 end
