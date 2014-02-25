0bfa5c1d9f43f4fb825cc88bc9058660a1faa83b
Zack Stayman <z.stayman@gmail.com>
2014-02-25 11:32:47 -0500

    fixed conflicts

--- spec/factories/artist.rb
+++ spec/factories/artist.rb
@@ -1,11 +1,6 @@
 FactoryGirl.define do
   factory :artist do
-<<<<<<< HEAD
-    name "kesha2"
-    photo_url "www.google.com"
-=======
     name { Faker::Name.name }
     photo_url "http://google.com"
   end
->>>>>>> 153844e3a5eaef553899a39f2e6d558477c1cea8
 end
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,16 +1,10 @@
 FactoryGirl.define do
   factory :user do
-<<<<<<< HEAD
-    sequence(:email) {|n| "j#{n}@k.co"}
-    first_name "Jeff"
-    last_name "K"
-=======
     email { Faker::Internet.email } 
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
     password "jeff"
     password_confirmation "jeff"
->>>>>>> 153844e3a5eaef553899a39f2e6d558477c1cea8
     dob Date.today
     balance 10.00
   end
